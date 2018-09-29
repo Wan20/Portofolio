5fba6e8a6412fa52813d4191cc5a368d2b1bd892
Wandy <Wan20@users.noreply.github.com>
2018-09-29 19:48:22 +0700

    Step 2 Add NavBar
    Co-Authored-By: wandy <owandywang@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//event pada saat link di klik
+$('.page-scroll').on('click', function(e){
+    var tujuan       = $(this).attr('href');
+    var elemenTujuan = $(tujuan);
+
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1200, 'easeInOutExpo');
+    e.preventDefault();
+});
